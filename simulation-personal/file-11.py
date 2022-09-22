b67013dbdbf147de1874a63c21cd810cbf066a5c
khizarsiddiqui <khizarsiddiqui10@gmail.com>
2022-09-23 00:40:27 +0500

    intializing with ideas

--- simulation-personal/file-11.py
+++ simulation-personal/file-11.py
@@ -1,2 +1,17 @@
 # will be doing simulation using concepts learned from books i studied
 # will be using matplotlib and numpy
+import matplotlib.pyplot as plt
+plt.style.use('seaborn-whitegrid')
+import numpy as np
+from mpl_toolkits.basemap import Basemap
+
+fig, ax = plt.subplots(1, 2, figsize=(12, 8))
+
+for i, res in enumerate(['l', 'h']):
+    m = Basemap(projection='gnom', lat_0=57.3, lon_0=-6.2,
+                width=90000, height=120000, resolution=res, ax=ax[i])
+    m.fillcontinents(color="#FFDDCC", lake_color='#DDEEFF')
+    m.drawmapboundary(fill_color="#DDEEFF")
+    m.drawcoastlines()
+    ax[i].set_title("resolution='{0}'".format(res))
+  
